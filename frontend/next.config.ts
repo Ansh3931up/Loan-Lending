765b9a3be5fca789a952fc63e71fae4ee5884fd3
Ansh <thebeliever39@gmail.com>
2025-02-15 11:18:24 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
+import path from 'path'
+
 const nextConfig = {
   images: {
     unoptimized: true,
@@ -13,10 +15,12 @@ const nextConfig = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      '@': __dirname,
-    };
-    return config;
-  },
+      '@': path.resolve(__dirname),
+      '@/components': path.resolve(__dirname, 'components'),
+      '@/lib': path.resolve(__dirname, 'lib')
+    }
+    return config
+  }
 }
 
-module.exports = nextConfig
+export default nextConfig
